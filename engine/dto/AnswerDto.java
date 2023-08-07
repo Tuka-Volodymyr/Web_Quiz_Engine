ceb1ead447bb6e676a6c4ec91244427e88d82104
Volodymyr <tukavolody@gmail.com>
2023-08-07 21:52:06 +0300

    Add answerDto class for json format

---
+++ engine/dto/AnswerDto.java
@@ -0,0 +1,12 @@
+package engine.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+public class AnswerDto {
+    private ArrayList<Integer> answer;
+}
