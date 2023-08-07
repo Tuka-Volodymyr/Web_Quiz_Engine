e09e06ed6baee106e5e07b400490a1c3391b2033
Volodymyr <tukavolody@gmail.com>
2023-08-07 22:09:53 +0300

    Add user not found exception

---
+++ engine/exceptions/UserNotFoundException404.java
@@ -0,0 +1,8 @@
+package engine.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "User not found!")
+public class UserNotFoundException404 extends RuntimeException{
+}
